8652f982f203f57c835ef4614fe6796fc6d8b221
Minh Duy <minhduy.truong@tu-dortmund.de>
2022-09-26 12:39:12 +0000

    fixed gcc issues regarding atomic operations

--- include/uk/bitops.h
+++ include/uk/bitops.h
@@ -281,7 +281,7 @@ __uk_test_and_clear_bit(long nr, volatile unsigned long *addr)
 	__u8 mask = 1 << (nr & 7);
 	__u8 orig;
 
-	//orig = __atomic_fetch_and(ptr, ~mask, __ATOMIC_RELAXED);
+	orig = __atomic_fetch_and(ptr, ~mask, __ATOMIC_RELAXED);
 
 	return (orig & mask) != 0;
 }
@@ -325,7 +325,7 @@ __uk_test_and_set_bit(long nr, volatile unsigned long *addr)
 	__u8 mask = 1 << (nr & 7);
 	__u8 orig;
 
-	//orig = __atomic_fetch_or(ptr, mask, __ATOMIC_RELAXED);
+	orig = __atomic_fetch_or(ptr, mask, __ATOMIC_RELAXED);
 
 	return (orig & mask) != 0; 
 }
